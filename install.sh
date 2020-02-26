49819f57c170c017f397c97b55d11209830ca8ec
shimoyamatomoaki <shimoyama@udstudio.co.jp>
2020-02-26 19:22:28 +0900

    [add] シンボリックリンク用のsh作成

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/vim/bundle/neosnippet-snippets/snippets/css.snip ~/.vim/bundle/neosnippet-snippets/snippets/css.snip
+ln -s ~/dotfiles/vim/bundle/neosnippet-snippets/snippets/vim.snip ~/.vim/bundle/neosnippet-snippets/snippets/vim.snip
+ln -s ~/dotfiles/.agignore ~/.agignore
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.hyper.js ~/.hyper.js
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
